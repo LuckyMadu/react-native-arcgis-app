3fe1ba781c767847020396fff453daefb3f4c048
Lahiru <lahiruamarathunga526@gmail.com>
2022-11-22 19:21:22 +0530

    initialized the project

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet
+} from 'react-native';
+import ArcGISMapView from 'react-native-arcgis-mapview'
+
+const App = () => {
+  const initialRegion = [{ latitude: 34.055561, longitude: -117.182602 }]
+
+  const mapView = useRef();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ArcGISMapView ref={mapView} style={styles.mapStyle}
+        initialMapCenter={initialRegion}
+        recenterIfGraphicTapped={true}
+        rotationEnabled={false}
+        mapBasemap={{ type: 'normal' }}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  mapStyle: {
+    width: '100%',
+    height: '100%'
+  }
+
+});
+
+export default App;
